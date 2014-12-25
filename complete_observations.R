03de8162e08d02a376a0b792a527b06190903c5c
Donne Martin <donne.martin@gmail.com>
2014-12-25 11:45:06 -0500

    Code cleanup

--- complete_observations.R
+++ complete_observations.R
@@ -11,11 +11,11 @@ CompleteObservations <- function(directory, id=1:332) {
   #
   # Returns:
   #   A data frame of the form:
-  #   id nobs
+  #   id numObs
   #   1  117
   #   2  1041
   #   ...
-  #   where 'id' is the monitor ID number and 'nobs' is the
+  #   where 'id' is the monitor ID number and 'numObs' is the
   #   number of complete cases
 
   source("utils.R")
@@ -24,38 +24,38 @@ CompleteObservations <- function(directory, id=1:332) {
 
   # Generate a data frame of observations per file
   dfObservations <- ldply(listOfDataFrames,
-                          function(x) {
-                            return(nrow(na.omit(x)))
+                          function(list) {
+                            return(nrow(na.omit(list)))
                           })
 
   # Generate a data frame of file ids
   dfFiles <- ldply(id,
-                   function(x) {
-                     return(x)
+                   function(id) {
+                     return(id)
                    })
 
+  # Combine the file ids and observations into a single data frame
   dfCompleteObservations <- data.frame(dfFiles, dfObservations)
 
-  # Rename the default columns to be more descriptive
-  dfCompleteObservations <- rename(dfCompleteObservations,
-                                   c("V1"="id", "V1.1"="nobs"))
+  # Change the column names to something more manageable
+  colnames(dfCompleteObservations) <- c("id", "numObs")
+
   return(dfCompleteObservations)
 }
 
 # Tests
-# TODO: Add RUnit or testthat unit test
 CompleteObservations("specdata", 1)
-# id nobs
+# id numObs
 # 1  1  117
 CompleteObservations("specdata", c(2, 4, 8, 10, 12))
-# id nobs
+# id numObs
 # 1  2 1041
 # 2  4  474
 # 3  8  192
 # 4 10  148
 # 5 12   96
 CompleteObservations("specdata", 30:25)
-# id nobs
+# id numObs
 # 1 30  932
 # 2 29  711
 # 3 28  475
@@ -63,5 +63,5 @@ CompleteObservations("specdata", 30:25)
 # 5 26  586
 # 6 25  463
 CompleteObservations("specdata", 3)
-# id nobs
+# id numObs
 # 1  3  243
